28b672c2c88d2c0c55393077e93f18018a5fdf10
Ben Porter <jammin1984@gmail.com>
2011-09-19 23:41:52 +0100

    vars into anon function example

--- lamdba.rb
+++ lamdba.rb
@@ -1,4 +1,8 @@
 ben = -> { puts "Porter" };
 ben.call;
 
+pass = lambda {|msg| "Survey says #{msg}"};
+puts pass.call("Nil points");
 
+shorthand = -> msg {"Survey says #{msg}"};
+puts shorthand.call("Ahh Ahh");
